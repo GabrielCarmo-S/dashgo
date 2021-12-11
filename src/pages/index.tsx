5e41a679654be103187b5913bacfa8e63495609a
GabrielCarmo-S <perien.op@gmail.com>
2021-12-11 16:40:22 -0300

    Criado header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Button, Flex, Stack, } from '@chakra-ui/react';
 import { Input } from '../components/Form/Input';
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column" >
