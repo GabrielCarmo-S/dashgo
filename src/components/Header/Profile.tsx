f8dee491f6cfe2bf1e7c6cc78f6bdf2088eadf16
GabrielCarmo-S <perien.op@gmail.com>
2021-12-13 20:29:46 -0300

    Ajustes na responsividade do header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gabriel Carmo</Text>
-        <Text color="gray.300" fontSize="small">gabriekcarmk@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gabriel Carmo</Text>
+          <Text color="gray.300" fontSize="small">gabriekcarmk@gmail.com</Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Gabriel Carmo" src="https://avatars.githubusercontent.com/u/70289493?s=400&u=9ef711cced6b061195c95e507dee5835be08d49c&v=4" />
     </Flex>
